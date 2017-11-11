59830677608a347c9e7916ac194cd987dea27b34
abecel22 <abe2211@gmail.com>
2017-11-10 23:51:49 -0600

    Updated Longest word with ES6.

--- Longest word in a string.js
+++ Longest word in a string.js
@@ -1,12 +1,12 @@
-
+//jshint esversion: 6
 
 function findLongestWord(str) {
-
-  var splitArray = str.split(" ");
-      var sortedArray = splitArray.sort(function(a,b){
-                      return b.length-a.length;
-                      });
-    return sortedArray[0].length;
-}
-
-console.log(findLongestWord("What if we try a super-long word such as otorhinolaryngology"));
+    
+      let array = str.split(' '); 
+     
+      array.sort((a,b) => b.length - a.length);
+     
+      return array[0].length; 
+    }
+    
+    findLongestWord("The quick brown fox jumped over the lazy dog");
